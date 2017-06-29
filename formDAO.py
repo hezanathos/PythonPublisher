8d434cae176d181d3cd1c1675e3334cb10c2bf27
Loummyy <lamyae.laaroussi@yahoo.com>
2017-06-29 14:32:04 +0200

    formulaire merge

--- formDAO.py
+++ formDAO.py
@@ -1,8 +1,10 @@
 # -*- coding:utf-8 -*-
-from flask import Flask,request
+from flask import Flask,request,flash
 from flaskext.mysql import MySQL
 import sys
+from pymysql.err import IntegrityError
 mysql = MySQL()
+
 
 app = Flask('Dynamique')
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -11,15 +13,19 @@ app.config['MYSQL_DATABASE_DB'] = 'siteweb_python'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-def formulaire(titre, taille_titre, chemin_image, article, var):
-	data = {"titre":titre,"taille_titre":taille_titre, "chemin_image":chemin_image, "article":article, "var"}
-	cursor = mysql.connect().cursor()
-	cursor.execute("""INSERT INTO pages_web (titre, taille_titre, chemin_image, article) VALUES(%(titre)s, %(taille_titre)s, %(chemin_image)s, %(article)s )""",data)
-	#data = cursor.fetchone()
-	if data is None:
-		print('data is none', file=sys.stderr)
-		return False
-	else:
-		print('data is not none', file=sys.stderr)
-		return True
+def formulaire(params):
+	#data = {"titre":titre,"taille_titre":taille_titre, "chemin_image":chemin_image, "article":article, "var"}
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	insertion="""INSERT INTO pages_web (article, titre, chemin_image, taille_titre, numero_page, user_mail) VALUES( %(article)s, %(titre)s, %(chemin_image)s, %(taille_titre)s, %(numero_page)s, %(user_mail)s )"""
+	try:
+		cursor.execute(insertion,params)
+		conn.commit()
 
+	except IntegrityError as e:
+		return False
+
+	cursor.close()
+
+	return True
+	 
