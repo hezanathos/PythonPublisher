14beb4feb5204c3556ea9a9dfad9113a07fe8629
yusfuM <yusuf.makanjuola@orange.com>
2017-07-04 14:48:33 +0200

    Added the function to input photos in articles

--- pageDAO.py
+++ pageDAO.py
@@ -21,7 +21,7 @@ def get(username,pagenumber):
 	page = {}
 	page["article"]="Pas d article"
 	page["titre"]="Pas de titre"
-	page["chemin"]=""
+	page["chemin_image"]=""
 	page["taille"]="1"
 
 	if data is None:
@@ -30,6 +30,6 @@ def get(username,pagenumber):
 		for row in data:
 			page["article"]=row[0]
 			page["titre"]=row[1]
-			page["chemin"]=row[2]
+			page["chemin_image"]=row[2]
 			page["taille"]=row[3]
 	return (page)
